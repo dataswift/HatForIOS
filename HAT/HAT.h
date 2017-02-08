ff73355792557c0538069ea5adbb08c2d88c4f4e
marios <marios4th@yahoo.gr>
2017-02-08 10:17:43 +0000

    Initial Commit

---
+++ HAT/HAT.h
@@ -0,0 +1,19 @@
+//
+//  HAT.h
+//  HAT
+//
+//  Created by Marios-Andreas Tsekis on 8/2/17.
+//  Copyright © 2017 Marios-Andreas Tsekis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HAT.
+FOUNDATION_EXPORT double HATVersionNumber;
+
+//! Project version string for HAT.
+FOUNDATION_EXPORT const unsigned char HATVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HAT/PublicHeader.h>
+
+
