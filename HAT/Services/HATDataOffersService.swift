a4344fc7b171607d4d587c55ea6b535b27966665
whiteshadow-gr <marios4th@yahoo.gr>
2017-06-26 17:25:02 +0100

    added ability to claim offers

--- HAT/Services/HATDataOffersService.swift
+++ HAT/Services/HATDataOffersService.swift
@@ -17,7 +17,7 @@ import UIKit
 
 public class HATDataOffersService: NSObject {
 
-    // MARK: - Get available data plugs
+    // MARK: - Get available data offers
     
     /**
      Gets the available data plugs for the user to enable
@@ -52,6 +52,48 @@ public class HATDataOffersService: NSObject {
                     }
                     
                     succesfulCallBack(returnValue, token)
+                } else {
+                    
+                    let message = NSLocalizedString("Server response was unexpected", comment: "")
+                    failCallBack(.generalError(message, statusCode, nil))
+                }
+            }
+        })
+    }
+    
+    // MARK: - Claim offer
+    
+    /**
+     Gets the available data plugs for the user to enable
+     
+     - parameter succesfulCallBack: A function of type ([HATDataPlugObject]) -> Void, executed on a successful result
+     - parameter failCallBack: A function of type (Void) -> Void, executed on an unsuccessful result
+     */
+    public class func claimOffer(applicationToken: String, offerID: String, succesfulCallBack: @escaping (String, String?) -> Void, failCallBack: @escaping (DataPlugError) -> Void) {
+        
+        let url: String = "http://databuyer.hubat.net/api/v1/offer/" + offerID + "/claim"
+        let headers: Dictionary<String, String> = ["X-Auth-Token": applicationToken]
+        
+        HATNetworkHelper.asynchronousRequest(url, method: .get, encoding: Alamofire.URLEncoding.default, contentType: ContentType.JSON, parameters: [:], headers: headers, completion: { (response: HATNetworkHelper.ResultType) -> Void in
+            
+            switch response {
+                
+            // in case of error call the failCallBack
+            case .error(let error, let statusCode):
+                
+                let message = NSLocalizedString("Server responded with error", comment: "")
+                failCallBack(.generalError(message, statusCode, error))
+            // in case of success call the succesfulCallBack
+            case .isSuccess(let isSuccess, let statusCode, let result, let token):
+                
+                if isSuccess {
+
+                    let dictionaryResponse = result.dictionaryValue
+                    
+                    if let claimed = dictionaryResponse["claimed"]?.stringValue {
+                        
+                        succesfulCallBack(claimed, token)
+                    }
                 } else {
                     
                     let message = NSLocalizedString("Server response was unexpected", comment: "")
