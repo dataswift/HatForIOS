0355585d66742e7f3b2a0d140fd257cfb5a6a467
whiteshadow-gr <marios4th@yahoo.gr>
2017-09-14 13:48:03 +0100

    trying to add caching capabilities out of the box

--- HAT/Objects/SystemStatus/HATSystemStatusObject.swift
+++ HAT/Objects/SystemStatus/HATSystemStatusObject.swift
@@ -14,11 +14,31 @@ import SwiftyJSON
 
 // MARK: Class
 
+public protocol HatApiType: Comparable {
+    
+    func toJSON() -> Dictionary<String, Any>
+    
+    mutating func initialize(fromCache: Dictionary<String, Any>)
+    
+    init()
+}
+
+extension HatApiType {
+    
+    public init(fromCache: Dictionary<String, Any>) {
+        
+        self.init()
+        
+        self.initialize(fromCache: fromCache)
+    }
+    
+}
+
 /// A class representing the system status object
-public struct HATSystemStatusObject: Comparable {
-
+public struct HATSystemStatusObject: HatApiType {
+    
     // MARK: - Comparable protocol
-
+    
     /// Returns a Boolean value indicating whether two values are equal.
     ///
     /// Equality is the inverse of inequality. For any values `a` and `b`,
@@ -28,10 +48,10 @@ public struct HATSystemStatusObject: Comparable {
     ///   - lhs: A value to compare.
     ///   - rhs: Another value to compare.
     public static func == (lhs: HATSystemStatusObject, rhs: HATSystemStatusObject) -> Bool {
-
+        
         return (lhs.title == rhs.title && lhs.kind == rhs.kind)
     }
-
+    
     /// Returns a Boolean value indicating whether the value of the first
     /// argument is less than that of the second argument.
     ///
@@ -43,41 +63,52 @@ public struct HATSystemStatusObject: Comparable {
     ///   - lhs: A value to compare.
     ///   - rhs: Another value to compare.
     public static func < (lhs: HATSystemStatusObject, rhs: HATSystemStatusObject) -> Bool {
-
+        
         return lhs.kind < rhs.kind
     }
-
+    
     // MARK: - Variables
-
+    
     /// The title of the object
     public var title: String = ""
     /// The kind object holding the values
     public var kind: HATSystemStatusKindObject = HATSystemStatusKindObject()
-
+    
     // MARK: - Initialisers
-
+    
     /**
      The default initialiser. Initialises everything to default values.
      */
     public init() {
-
+        
         title = ""
         kind = HATSystemStatusKindObject()
     }
-
+    
     /**
      It initialises everything from the received JSON file from the HAT
      */
     public init(from dictionary: Dictionary<String, JSON>) {
-
+        
         self.init()
-
+        
+        initialize(from: dictionary)
+    }
+    
+    public mutating func initialize(fromCache: Dictionary<String, Any>) {
+        
+        let dictionary = JSON(fromCache)
+        self.initialize(from: dictionary.dictionaryValue)
+    }
+    
+    public mutating func initialize(from dictionary: Dictionary<String, JSON>) {
+        
         if let tempTitle = dictionary["title"]?.stringValue {
-
+            
             title = tempTitle
         }
         if let tempKind = dictionary["kind"]?.dictionaryValue {
-
+            
             kind = HATSystemStatusKindObject(from: tempKind)
         }
     }
