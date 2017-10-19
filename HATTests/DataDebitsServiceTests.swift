8ac1bcd0f150ecd73ae61e8ed8e609b780a81a81
whiteshadow-gr <marios4th@yahoo.gr>
2017-10-19 15:58:07 +0100

    updated data offers and data debits links to v2

---
+++ HATTests/DataDebitsServiceTests.swift
@@ -0,0 +1,120 @@
+/**
+ * Copyright (C) 2017 HAT Data Exchange Ltd
+ *
+ * SPDX-License-Identifier: MPL2
+ *
+ * This file is part of the Hub of All Things project (HAT).
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/
+ */
+
+import Alamofire
+import Mockingjay
+import SwiftyJSON
+import XCTest
+
+internal class DataDebitsServiceTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testGetDataDebit() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        let body: [Dictionary<String, Any>] = [
+            [
+                "dataDebitKey": "8127e4c8-19e2-4e4e-aafb-1145bb175325",
+                "dateCreated": "2017-09-28T14:21:15+0000",
+                "client": [
+                    "userId": "8127e4c8-19e2-4e4e-aafb-1145bb175325",
+                    "email": "location-test-offer",
+                    "pass": "$2a$12$iJ2ZNq1.JBobqH4bsrn8b.wI6BJGFlvptkIdKrON5ALaB3enDEKvm",
+                    "name": "Location test offer",
+                    "role": "validate",
+                    "roles": []
+                ],
+                "bundles": [
+                    [
+                        "dateCreated": "2017-09-28T14:21:15+0000",
+                        "startDate": "2017-09-28T14:21:38+0000",
+                        "endDate": "2017-09-29T14:21:38+0000",
+                        "rolling": false,
+                        "enabled": true,
+                        "bundle": [
+                            "name": "8127e4c8-19e2-4e4e-aafb-1145bb175325",
+                            "bundle": [
+                                "facebook/posts": [
+                                    "endpoints": [
+                                        [
+                                            "endpoint": "facebook/posts",
+                                            "mapping": [
+                                                "story": "data.story",
+                                                "picture": "data.picture",
+                                                "lastUpdated": "data.lastUpdated",
+                                                "status_type": "data.status_type",
+                                                "full_picture": "data.full_picture"
+                                            ],
+                                            "filters": [
+                                                [
+                                                    "field": "data.story",
+                                                    "transformation": [
+                                                        "transformation": "searchable"
+                                                    ],
+                                                    "operator": [
+                                                        "search": "BBC",
+                                                        "operator": "find"
+                                                    ]
+                                                ]
+                                            ]
+                                        ]
+                                    ]
+                                ]
+                            ]
+                        ]
+                    ]
+                ]
+            ]
+        ]
+        
+        let userDomain = "testing.hubat.net"
+        let urlToConnect = "http://testing.hubat.net/api/v2/data-debit"
+        let token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOiJleUp3Y205MmFXUmxja2xFSWpvaVkzSmxaR1Z1ZEdsaGJITWlMQ0p3Y205MmFXUmxja3RsZVNJNkluUmxjM1JwYm1jaWZRPT0iLCJyZXNvdXJjZSI6InRlc3RpbmcuaHViYXQubmV0IiwiYWNjZXNzU2NvcGUiOiJvd25lciIsImlzcyI6InRlc3RpbmcuaHViYXQubmV0IiwiZXhwIjoxNTA4NjY4MjMwLCJpYXQiOjE1MDg0MDkwMzAsImp0aSI6IjZmYTNiYTNmN2QyMzMwMzMwMmUxMTgwMDllNzBjMzZmNGZiMjk3ODQzNmU4MDI0NmJmNTdjNmRlYWE1MjgxZGZmNGQwNjFhY2U4MjY5YjRiOTI5N2Q5ZmMyYmJkZjk5NDE2NzJmMGQ5MGM1YzFhMTkyYjZlNjk0N2Y3OWIxYzZjM2E5MmNlMGZmYzIyYTExMmI1NDJlYWY3ZmNkNTEzYjUzMzU1YmU4NzZjMTc0NmZlN2JlNTE1NmNmMjkzNDA5MDcwMTk4OTliOWYxMjEzMTMyNjNjMzBjYjI3MmViZTgwMjI1MDg0YmFkOGNkNTgzMzZkOWQ4YzIzNDkzYWM1YzIifQ.cObRGNjdXfjfp9pd5SF2vS-gj5t7faEEdn5bVat1TvjnX60oQGdc55M80VuDaNXnTnLQOAL6x9YnWgDY1bnmRa0_4_W8553VmMbEWdQ6fDaeW9iLhkh-UYWpIlxexvdof8Vy7YwKTLseR0l7JhheWBQkugp7bAJ7ZTnAIDOfGDR02_i7cdASvJwbefIeSOahdx9DVPMUj1DRE2dq7cR2-eRqJyc09cITKtKu0fY8reHqRfc2151grGx7KxRNd2aBMnqJeNgscOZR13jshapmrDTo9SSpyo8Z6CKcoWj-_7NdHa-8ceJSZzL64JkRbvCX07HdpmAuSIHuAA0xuQIqlg"
+        
+        let expectationTest = expectation(description: "Checking getting of data debits...")
+        
+        MockingjayProtocol.addStub(matcher: http(.get, uri: urlToConnect), builder: json(body))
+        
+        func completion(dataDebits: [DataDebitObject], newToken: String?) {
+            
+            XCTAssertTrue(!dataDebits.isEmpty)
+            expectationTest.fulfill()
+        }
+        
+        func failed(error: DataPlugError) {
+            
+        }
+        
+        HATDataDebitsService.getAvailableDataDebits(
+            userToken: token,
+            userDomain: userDomain,
+            succesfulCallBack: completion,
+            failCallBack: failed)
+        
+        waitForExpectations(timeout: 10) { error in
+            
+            if let error = error {
+                print("Error: \(error.localizedDescription)")
+            }
+        }
+    }
+}
--- HATTests/DataPlugsServiceTests.swift
+++ HATTests/DataPlugsServiceTests.swift
@@ -187,24 +187,72 @@ internal class DataPlugsServiceTests: XCTestCase {
     }
 
     func testCheckDataDebit() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
 
-        let body: Dictionary<String, String> = [
-            "enabled": "true"
+        let body: Dictionary<String, Any> =
+            [
+                "dataDebitKey": "8127e4c8-19e2-4e4e-aafb-1145bb175325",
+                "dateCreated": "2017-09-28T14:21:15+0000",
+                "client": [
+                    "userId": "8127e4c8-19e2-4e4e-aafb-1145bb175325",
+                    "email": "location-test-offer",
+                    "pass": "$2a$12$iJ2ZNq1.JBobqH4bsrn8b.wI6BJGFlvptkIdKrON5ALaB3enDEKvm",
+                    "name": "Location test offer",
+                    "role": "validate",
+                    "roles": []
+                ],
+                "bundles": [
+                    [
+                        "dateCreated": "2017-09-28T14:21:15+0000",
+                        "startDate": "2017-09-28T14:21:38+0000",
+                        "endDate": "2017-09-29T14:21:38+0000",
+                        "rolling": false,
+                        "enabled": true,
+                        "bundle": [
+                            "name": "8127e4c8-19e2-4e4e-aafb-1145bb175325",
+                            "bundle": [
+                                "facebook/posts": [
+                                    "endpoints": [
+                                        [
+                                            "endpoint": "facebook/posts",
+                                            "mapping": [
+                                                "story": "data.story",
+                                                "picture": "data.picture",
+                                                "lastUpdated": "data.lastUpdated",
+                                                "status_type": "data.status_type",
+                                                "full_picture": "data.full_picture"
+                                            ],
+                                            "filters": [
+                                                [
+                                                    "field": "data.story",
+                                                    "transformation": [
+                                                        "transformation": "searchable"
+                                                    ],
+                                                    "operator": [
+                                                        "search": "BBC",
+                                                        "operator": "find"
+                                                    ]
+                                                ]
+                                            ]
+                                        ]
+                                    ]
+                                ]
+                            ]
+                        ]
+                    ]
+                ]
         ]
         let expectationTest = expectation(description: "Checking data debit...")
 
         let dataDebitID = "123"
         let userDomain = "mariostsekis.hubofallthings.net"
-        let urlToConnect = "https://" + userDomain + "/dataDebit/" + dataDebitID
+        let urlToConnect = "https://\(userDomain)/api/v2/data-debit/\(dataDebitID)"
         let appToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOiJleUp3Y205MmFXUmxja2xFSWpvaWJXRnlhVzl6ZEhObGEybHpMbWgxWW05bVlXeHNkR2hwYm1kekxtNWxkQ0lzSW5CeWIzWnBaR1Z5UzJWNUlqb2liV0Z5YVc5emRITmxhMmx6SW4wPSIsInJlc291cmNlIjoibWFyaW9zdHNla2lzLmh1Ym9mYWxsdGhpbmdzLm5ldCIsImFjY2Vzc1Njb3BlIjoib3duZXIiLCJpc3MiOiJtYXJpb3N0c2VraXMuaHVib2ZhbGx0aGluZ3MubmV0IiwiZXhwIjoxNDg3MzIyNzk0LCJpYXQiOjE0ODcwNjM1OTQsImp0aSI6ImUxYWY1ODY3ZWRhNjFmM2MxMmE3YzE1OGEwNDhmMjM0YmFiMzI3ZDVhNzQ5NDIzYWIwNGU1OTkxZTUxZDE1MTM0MzE3MDQwZDFhMjBiNTI1ZDMxODFmNWJiNTI3ZmVkMWJhMWYzZWEwZTlmZTM0MjZmM2E5ZDMwNmFjMGY3NGFjMTM1MWQ1OTFhYmMxZTI4NmJmMGYyMjgzNzRkZWU2MDdhYWQ2MjU3OGJkNzJhZTI2OWI4NDY4NWJiYjY2OGMzMmQzODRkZjQwZjIxNDU4Y2IwMjFlMDc5ODc5MzFmNmVlNTMyNWMxNGViNGNiOGFmYTNlMWI0ZjgwNzQ5M2M3ZDYifQ.lz3Snzglz9WtGTIlp4qmJsCnpljrwafYRSg7QKa9CNQAfq_yB5XIOcfH8As8f_fneQW08-ats4Qk1F_yfeQKPIa2GnissQj0W2rl4pnRMiFcKE2vddMRsM_fwGEsr43foGNIjJM3KIBPaECxC_QZdGdqu_wnpSS2rRqbJPrcdPs5FOhAWaLdL6ej0vkhdVX97-VwGyW70AcwZ-yFP8mKLZygwixqPn1-ubCc2ahkS94cM40s4-fon0HNNC4SNOB-q4g_87caAjXRN6cchrJitltHZ3_4xe4p9wMCK-LGjF99xUYT4aUbsiJ4tOPKOcqQsZgbfBZGqUM4_4aHQQ3Pxg"
 
         MockingjayProtocol.addStub(matcher: http(.get, uri: urlToConnect), builder: json(body))
 
-        func success(result: String) {
+        func success(result: Bool) {
 
-            XCTAssertTrue(result == "enabled")
+            XCTAssertTrue(result)
             expectationTest.fulfill()
         }
 
